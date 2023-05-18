f36b4358f5c8ab57f2949474df103bcc2c32f160
Filipe Bacof <filipebacof@gmail.com>
2023-05-18 07:26:21 -0300

    env var + components

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,24 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito por{' '}
+      <a
+        target="_blank"
+        href="https://portifolio-filipe-bacof.vercel.app/"
+        className="underline hover:text-gray-100"
+        rel="noreferrer"
+      >
+        Filipe Bacof
+      </a>{' '}
+      no NLW da{' '}
+      <a
+        target="_blank"
+        href="https://rocketseat.com.br/"
+        className="underline hover:text-gray-100"
+        rel="noreferrer"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
